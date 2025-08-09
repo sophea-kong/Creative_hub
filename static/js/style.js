f6763f97adfefbb9361a51a189f2be2ca1630939
Sophea Kong <sopheakongs001@gmail.com>
2025-08-09 21:36:46 +0700

    nav bar improvement + about me content

--- static/js/style.js
+++ static/js/style.js
@@ -1,3 +1,28 @@
-function scrollDown(){
+function scrollDownAbout(){
     document.getElementById('about-me-id').scrollIntoView({ behavior: 'smooth', block: 'start'});
 }
+function scrollDownHome(){
+    document.getElementById('home-id').scrollIntoView({ behavior: 'smooth', block: 'start'});
+}
+// function scrollDownProject(){
+//     document.getElementById('home-id').scrollIntoView({ behavior: 'smooth', block: 'start'});
+// }
+// function scrollDownContact(){
+//     document.getElementById('home-id').scrollIntoView({ behavior: 'smooth', block: 'start'});
+// }
+
+function activateButton(buttonid,targetid){
+    window.addEventListener('scroll', function () {
+        const button = document.getElementById(buttonid);
+        const target = document.getElementById(targetid);
+        const targetPosition = target.getBoundingClientRect().top;
+
+        if (targetPosition < window.innerHeight && targetPosition > 0) {
+            button.classList.add('green');
+        } else {
+            button.classList.remove('green');
+        }
+    })
+}
+activateButton("aboutButton",'about-me-id')
+activateButton('homeButton','hello-id')
